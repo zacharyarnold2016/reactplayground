75146708f2611dd317672ee6b2ba4f3aac9665c2
Zachary Arnold <zacharyarnold2016@gmail.com>
2023-02-15 12:39:36 -0500

    feat(next): begins next implementation

--- netflix/src/components/App.tsx
+++ netflix/src/components/App.tsx
@@ -1,49 +1,50 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import TopBar from "./top/TopBar";
 import VideoBar from "./main/VideoBar";
-import { ErrorBoundary } from "react-error-boundary";
+import {ErrorBoundary} from "react-error-boundary";
 import ErrorFallback from "./err/ErrorFallback";
 import EditMovie from "./filmInteraction/edit/EditMovie";
 import DeleteMovie from "./filmInteraction/delete/DeleteMovie";
 import AddMovie from "./filmInteraction/add/AddMovie";
 import Details from "./details/Details";
-import { useSelector } from "react-redux";
-import { useSearchParams } from "react-router-dom";
-import { fetchDetails } from "../helpers/fetchDetails";
+import {useSelector} from "react-redux";
+import {useSearchParams} from "react-router-dom";
+import {fetchDetails} from "../helpers/fetchDetails";
 
 const App: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [searchParams, _setSearchParams] = useSearchParams();
-  const { form } = useSelector((state: any) => state.forms);
-  const movieId = searchParams.get("movieId");
-  const [details, setDetails] = useState(undefined);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [searchParams, _setSearchParams] = useSearchParams();
+    const {form} = useSelector((state: any) => state.forms);
+    const movieId = searchParams.get("movieId");
+    const [details, setDetails] = useState(undefined);
 
-  const fetch = useCallback(
-    () => fetchDetails({ movieId, setDetails }),
-    [movieId]
-  );
+    const fetch = useCallback(
+        () => fetchDetails({movieId, setDetails}),
+        [movieId]
+    );
 
-  useEffect(() => {
-    fetch();
-  }, [movieId, fetch]);
+    useEffect(() => {
+        fetch();
+    }, [movieId, fetch]);
 
-  return (
-    <div className="App">
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
-        <div className={form ? "blur" : ""}>
-          {details ? (
-            <Details details={details} callback={setDetails} />
-          ) : (
-            <TopBar />
-          )}
-          <VideoBar callback={setDetails} />
+    return (
+        <div className="App">
+            <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {
+            }}>
+                <div className={form ? "blur" : ""}>
+                    {details ? (
+                        <Details details={details} callback={setDetails}/>
+                    ) : (
+                        <TopBar/>
+                    )}
+                    <VideoBar callback={setDetails}/>
+                </div>
+                {form === "add" && <AddMovie/>}
+                {form === "edit" && <EditMovie/>}
+                {form === "delete" && <DeleteMovie/>}
+            </ErrorBoundary>
         </div>
-        {form === "add" && <AddMovie />}
-        {form === "edit" && <EditMovie />}
-        {form === "delete" && <DeleteMovie />}
-      </ErrorBoundary>
-    </div>
-  );
+    );
 };
 
 export default App;
---
+++ netflix/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import store from "../redux/store";
+//import "../index.css";
+import { router } from "../router/router";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} fallbackElement={<h1>LOADING</h1>} />
+    </Provider>
+  </React.StrictMode>
+);
